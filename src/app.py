d471380a863b4d48b3eafe9668f8846ed77f4736
orchinia <orchiniasuefiion@gmail.com>
2025-07-14 16:51:27 +0800

    add argparse to flask command

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request
+import argparse
 
+from flask import Flask, render_template, request
 from models import calculate_result, db, load_quiz_questions_optimized
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@mysql/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-DEV = True
+parser = argparse.ArgumentParser(description="Run the Flask app with optional debug mode.")
+parser.add_argument('--debug', action='store_true', help="Run the app in debug mode")
+args = parser.parse_args()
 
 db.init_app(app)
 
@@ -29,4 +32,4 @@ def submit():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8888, debug=DEV)
+    app.run(host='0.0.0.0', port=8888, debug=args.debug)
